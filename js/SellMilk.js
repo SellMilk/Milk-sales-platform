f9fd074136afdb7d0ee8f225c8ae7acaac087aed
zzzzzzb <503684754@qq.com>
2017-05-19 19:42:56 +0800

    zzb修改

--- js/SellMilk.js
+++ js/SellMilk.js
@@ -1,3 +1,10 @@
 /**
  * Created by Administrator on 2017/5/19.
  */
+$(function () {
+    /*热词选择开始*/
+    $('.home-header .hot-word').on('click',function () {
+        $('.home-header input').val($(this).html());
+    })
+    /*热词选择结束*/
+});
